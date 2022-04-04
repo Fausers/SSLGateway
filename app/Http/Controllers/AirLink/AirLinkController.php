ca3f8ac6faa200f13ede19d3a15a8827dbe1b7fa
Fausers <felixthobias@gmail.com>
2022-04-04 04:30:29 +0300

    Link

--- app/Http/Controllers/AirLink/AirLinkController.php
+++ app/Http/Controllers/AirLink/AirLinkController.php
@@ -3,12 +3,34 @@
 namespace App\Http\Controllers\AirLink;
 
 use App\Http\Controllers\Controller;
+use App\Models\AirLink\AirLinkData;
+use App\Models\Payment\AirtelData;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Http;
 
 class AirLinkController extends Controller
 {
-     public function addDevice($data = null)
+
+    public function login()
+    {
+        $air_link = AirLinkData::first();
+        if ($air_link == null){
+            $air_link = new AirLinkData;
+            $air_link->app_url = "https://airlink.enaccess.org/api/auth/token";
+            $air_link->username = "webi@simusolar.com";
+            $air_link->password = "G*h!Qnt#2QVf4xw";
+            $air_link->save();
+        }
+
+        return $response = Http::withHeaders([
+            'Content-Type' => 'application/json'
+        ])->post($air_link->app_url, [
+            'username' => $air_link->username,
+            'password' => $air_link->password,
+        ]);
+    }
+
+    public function addDevice($data = null)
     {
         return $response = Http::withHeaders([
             'X-Authorization' => 'Bearer eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJ3ZWJpQHNpbXVzb2xhci5jb20iLCJzY29wZXMiOlsiVEVOQU5UX0FETUlOIl0sInVzZXJJZCI6ImUwYjJjYjMwLTg5YzEtMTFlYy05NWI2LTExYjU4NjZkMGMyMiIsImZpcnN0TmFtZSI6IldlYmkiLCJlbmFibGVkIjp0cnVlLCJpc1B1YmxpYyI6ZmFsc2UsInRlbmFudElkIjoiOTFlYjE0YzAtZjQ2Yy0xMWViLTlkNDktYzczNzc4OGI1Mzk5IiwiY3VzdG9tZXJJZCI6IjEzODE0MDAwLTFkZDItMTFiMi04MDgwLTgwODA4MDgwODA4MCIsImlzcyI6InRoaW5nc2JvYXJkLmlvIiwiaWF0IjoxNjQ5MDEzNjczLCJleHAiOjE2NDkwMjI2NzN9.w0ew42XVPRzhdGU5qybMEIQnYwYG5sXDrqcKsz2e7j9hFUbPWA-EWBkmf6eV8fR__oCG69T7FCVUxcqfFTyU_A',
---
+++ database/migrations/2022_04_04_010334_create_air_link_data_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateAirLinkDataTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('air_link_data', function (Blueprint $table) {
+            $table->id();
+            $table->string('app_url');
+            $table->string('username');
+            $table->string('password');
+            $table->string('token')->nullable();
+            $table->string('refresh_token')->nullable();
+            $table->string('profile_id')->nullable();
+            $table->string('group_id')->nullable();
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('air_link_data');
+    }
+}
