e06ed79ea7e109d662fbc0cab5637b69929ee344
Fausers <felixthobias@gmail.com>
2022-05-14 08:34:53 +0300

    Webi

--- app/Http/Controllers/Webi/WebiController.php
+++ app/Http/Controllers/Webi/WebiController.php
@@ -10,7 +10,7 @@ class WebiController extends Controller
 {
     public function updateSMS(Request $request)
     {
-        $url = "https://api.ninox.com/v1/teams/tBEzT47PPxBqkK3n2/databases/s09bhyujje50/tables/P/records/".$request["webi_id"];
+        $url = "http://api.ninox.com/v1/teams/tBEzT47PPxBqkK3n2/databases/s09bhyujje50/tables/P/records/".$request["webi_id"];
 
         $data = [
             'fields'=>[
